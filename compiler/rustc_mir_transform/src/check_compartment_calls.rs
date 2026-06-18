641562a9d144ff68cc5346305026c53a379a6014
uneeb125 <uneebkamal125@gmail.com>
2026-06-18 15:20:00 -0500

    Added CompartmentViolation assert before virtual calls

--- compiler/rustc_mir_transform/src/check_compartment_calls.rs
+++ compiler/rustc_mir_transform/src/check_compartment_calls.rs
@@ -1,5 +1,7 @@
+use rustc_index::IndexVec;
+use rustc_middle::mir::interpret::Scalar;
 use rustc_middle::mir::*;
-use rustc_middle::ty::TyCtxt;
+use rustc_middle::ty::{self, Instance, TyCtxt};
 use rustc_session::Session;
 use tracing::debug;
 
@@ -24,20 +26,88 @@ impl<'tcx> crate::MirPass<'tcx> for CheckCompartmentCalls {
             return;
         }
 
+        let typing_env = body.typing_env(tcx);
         let basic_blocks = body.basic_blocks.as_mut();
+        let local_decls = &mut body.local_decls;
 
-        // Count calls for logging; full check injection coming in next iteration
-        let mut call_count = 0;
-        for (bb, data) in basic_blocks.iter().enumerate() {
-            let Some(ref terminator) = data.terminator else { continue; };
-            if let TerminatorKind::Call { .. } = &terminator.kind {
-                call_count += 1;
-                debug!("CheckCompartmentCalls: call in bb{bb}");
-            }
+        // Iterate backwards to avoid index invalidation after block splits
+        for bb in basic_blocks.indices().rev() {
+            let Some(ref terminator) = basic_blocks[bb].terminator else { continue; };
+            let TerminatorKind::Call {
+                func: Operand::Constant(box ConstOperand { const_, .. }),
+                ..
+            } = &terminator.kind
+            else {
+                continue;
+            };
+
+            let ty::FnDef(def_id, args) = *const_.ty().kind() else { continue; };
+
+            let Ok(Some(instance)) = Instance::try_resolve(tcx, typing_env, def_id, args)
+            else {
+                continue;
+            };
+
+            let ty::InstanceKind::Virtual(trait_def_id, vtable_index) = instance.def else {
+                continue;
+            };
+
+            debug!(
+                "CheckCompartmentCalls: virtual call in bb{bb:?} trait={trait_def_id:?} idx={vtable_index}"
+            );
+
+            let source_info = terminator.source_info;
+
+            // Split block: put the call terminator into a new block
+            let new_bb = split_block(basic_blocks, Location {
+                block: bb,
+                statement_index: basic_blocks[bb].statements.len(),
+            });
+
+            // Inject a placeholder true assert for v1.
+            // v2 will load callee compartment from vtable and compare with caller_id.
+            let block_data = &mut basic_blocks[bb];
+            let ok_local = local_decls
+                .push(LocalDecl::with_source_info(tcx.types.bool, source_info))
+                .into();
+
+            let const_true = Operand::Constant(Box::new(ConstOperand {
+                span: source_info.span,
+                user_ty: None,
+                const_: Const::Val(
+                    ConstValue::Scalar(Scalar::from_bool(true)),
+                    tcx.types.bool,
+                ),
+            }));
+
+            block_data.statements.push(Statement::new(
+                source_info,
+                StatementKind::Assign(Box::new((ok_local, Rvalue::Use(const_true)))),
+            ));
+
+            block_data.terminator = Some(Terminator {
+                source_info,
+                kind: TerminatorKind::Assert {
+                    cond: Operand::Copy(ok_local),
+                    expected: true,
+                    target: new_bb,
+                    msg: Box::new(AssertKind::CompartmentViolation),
+                    unwind: UnwindAction::Unreachable,
+                },
+            });
         }
-        debug!(
-            "CheckCompartmentCalls: {call_count} call(s) in {:?}",
-            body.source.def_id()
-        );
     }
+}
+
+fn split_block(
+    basic_blocks: &mut IndexVec<BasicBlock, BasicBlockData<'_>>,
+    location: Location,
+) -> BasicBlock {
+    let block_data = &mut basic_blocks[location.block];
+    let new_block = BasicBlockData::new_stmts(
+        block_data.statements.split_off(location.statement_index),
+        block_data.terminator.take(),
+        block_data.is_cleanup,
+    );
+    basic_blocks.push(new_block)
 }
